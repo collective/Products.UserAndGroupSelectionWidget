44044fb86dc4d63884b59e76b0314a4cb95aad42
jensens <jensens@12379f12-61e2-0310-a953-c5ab0a7db1c5>
2009-10-01 21:18:11 +0000

    small cleanup

--- src/Products/UserAndGroupSelectionWidget/browser.py
+++ src/Products/UserAndGroupSelectionWidget/browser.py
@@ -1,42 +1,13 @@
-# -*- coding: utf-8 -*-
-#
-# GNU General Public License (GPL)
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.
-#
-
-__author__ = """Robert Niederreiter <robertn@bluedynamics.com>"""
-__docformat__ = 'plaintext'
-
 import types
-
 from zope.interface import implements
 from Products.Five import BrowserView
-
 from Products.PlonePAS.interfaces.group import IGroupIntrospection
-
 from ZTUtils import make_query
 from Products.CMFCore.utils import getToolByName
-
 from interfaces import IUserAndGroupSelectView
 from interfaces import IUserAndGroupSelectPopupView
-
 from memberlookup import MemberLookup
 from alphabatch import AlphaBatch
-
 
 class UserAndGroupSelectView(BrowserView):
     """See interfaces.IUserAndGroupSelectView for documentation details.
--- src/Products/UserAndGroupSelectionWidget/widget.py
+++ src/Products/UserAndGroupSelectionWidget/widget.py
@@ -1,31 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright (c) 2007 by BlueDynamics Alliance, Austria
-#
-# initially this code was derived from ATMemberselectWidget
-#
-# GNU General Public License (GPL)
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.
-#
-
-__author__ = """Jens Klein <jens@bluedynamics.com>,
-                Robert Niederreiter <robertn@bluedynamcis.com>"""
-__docformat__ = 'plaintext' 
-
 import types
 from zope.component import ComponentLookupError
 from Globals import InitializeClass
@@ -41,7 +13,8 @@ class UserAndGroupSelectionWidget(TypesWidget):
         'macro'                 : "userandgroupselect",
         'helper_js'             : ('userandgroupselect.js',),
         'size'                  : 7,    # size of form-element taking the users
-        'groupName'             : '',    # takes the given group as default, a group id
+        'groupName'             : '',    # takes the given group as default, 
+                                         # a group id
         'usersOnly'             : False, # only allow user selection
         'groupsOnly'            : False, # allow only group selection
         'groupIdFilter'         : '*',   # allow all groups
@@ -50,7 +23,8 @@ class UserAndGroupSelectionWidget(TypesWidget):
     security = ClassSecurityInfo()    
 
     security.declarePublic('process_form')
-    def process_form(self, instance, field, form, empty_marker=None, emptyReturnsMarker=None,):
+    def process_form(self, instance, field, form, empty_marker=None, 
+                     emptyReturnsMarker=None,):
         """process the form data and return it."""
         result = TypesWidget.process_form (self, instance, field, form, 
                                            empty_marker, emptyReturnsMarker, )
@@ -58,7 +32,8 @@ class UserAndGroupSelectionWidget(TypesWidget):
             return result
         value, kwargs = result
         
-        # The widget always returns a empty item (strange) when we use the multival option.
+        # The widget always returns a empty item (strange) when we use the 
+        # multival option.
         # Remove the empty items manually
         if type(value) is types.ListType:
             value = [item for item in value if item]
@@ -81,9 +56,9 @@ class UserAndGroupSelectionWidget(TypesWidget):
         return groupid
             
 
-registerWidget(UserAndGroupSelectionWidget,
-               title='User and Group Selection Widget',
-               description=('You can select users searched from a popup window.'),
-               used_for=('Products.Archetypes.Field.LinesField',
-	                     'Products.Archetypes.Field.StringField', )
-               )
+registerWidget(
+    UserAndGroupSelectionWidget,
+    title='User and Group Selection Widget',
+    description=('You can select users searched from a popup window.'),
+    used_for=('Products.Archetypes.Field.LinesField',
+	          'Products.Archetypes.Field.StringField', ))
