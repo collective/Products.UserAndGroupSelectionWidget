fd3f8ae93a897ef06f47700ddbe0f7b748478eb1
jensens <jensens@12379f12-61e2-0310-a953-c5ab0a7db1c5>
2009-10-01 20:33:15 +0000

    copy back in place

---
+++ src/Products/UserAndGroupSelectionWidget/memberlookup.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+#
+# GNU General Public License (GPL)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+
+__author__ = """Robert Niederreiter <robertn@bluedynamics.com>"""
+__docformat__ = 'plaintext'
+
+import time
+import types
+from zope.component import ComponentLookupError
+
+from Products.CMFCore.utils import getToolByName
+from Products.PlonePAS.interfaces.group import IGroupIntrospection
+
+from bda.cache import ICacheManager
+from bda.cache import Memcached
+ 
+from interfaces import IGenericGroupTranslation
+from interfaces import IGenericFilterTranslation
+
+# make this dynamic, XXX
+CACHEPROVIDER = Memcached(['127.0.0.1:11211'])
+
+class MemberLookup(object):
+    """This object contains the logic to list and search for users and groups.
+    """
+    
+    def __init__(self, context, request, widget):
+        """Construct this object and do base initialization.
+        """
+        self.context = context
+        self.widget = widget
+        self.searchabletext = request.get('searchabletext', '')
+        group = request.get('selectgroup', '')
+        try:
+            grouptranslation = IGenericGroupTranslation(self.context)
+            self.currentgroupid = grouptranslation.translateToRealGroupId(group)
+        except ComponentLookupError:
+            self.currentgroupid = group
+        except TypeError, e:
+            if e[0] == 'Could not adapt':
+                self.currentgroupid = group
+            else:
+                raise
+        return            
+        
+    def getGroups(self):
+        """Return the groups.
+        """
+        #start = time.time()
+        filter = self._allocateFilter()
+        aclu = getToolByName(self.context, 'acl_users')
+        groups = aclu.getGroups()
+        ret = []
+        for group in groups:
+            gid = group.getId()
+            if not self._groupIdFilterMatch(gid, filter):
+                continue
+            ret.append((gid, group.getGroupTitleOrName()))
+        #print 'getGroups took %s' % str(time.time() - start)
+        return ret
+        
+    def getMembers(self):
+        """Return the Users in the following form.
+        
+        {
+            'id': 'mmustermann',
+            'fullname': 'Max Mustermann',
+        }
+        """
+        #start = time.time()
+        filter = self._allocateFilter()
+        group = self.currentgroupid
+        if group != 'ignore' and group != '':
+            key = 'userandgroupselectionwidget:%s' % group
+            manager = ICacheManager(CACHEPROVIDER)
+            users = manager.getData(self._readGroupMembers, key, args=[group])
+        else:
+            users = self._searchUsers()
+        reduce = True
+        for fil in filter:
+            if fil == '*':
+                reduce == False
+        if reduce:
+            users = self._reduceMembers(users, filter)
+        #print 'getMembers took %s' % str(time.time() - start)
+        return users
+    
+    def _getUserIdsOfGroup(self, groupid):
+        aclu = getToolByName(self.context, 'acl_users')
+        for id, giplugin in aclu.plugins.listPlugins(IGroupIntrospection):
+            userids = giplugin.getGroupMembers(groupid)
+            if userids: 
+                return userids
+        return []
+    
+    def _readGroupMembers(self, gid):
+        aclu = getToolByName(self.context, 'acl_users')
+        user_ids = self._getUserIdsOfGroup(gid)
+        return self._getUserDefs(user_ids)
+    
+    def _searchUsers(self):
+        st = self.searchabletext
+        # TODO: Search is done over all available groups, not only over groups
+        # which should be applied. also see getGroups.
+        if len(st) < 3:
+            return []
+        aclu = getToolByName(self.context, 'acl_users')
+        users_dict = aclu.searchUsers(name=st)
+        user_ids = [user['id'] for user in users_dict]
+        return self._getUserDefs(user_ids)
+    
+    def _getUserDefs(self, uids):
+        aclu = getToolByName(self.context, 'acl_users')
+        users = [aclu.getUserById(user_id) for user_id in uids]
+        ret = []
+        for user in users:
+            if user is None:
+                continue
+            user_id = user.getId()
+            for psheet in user.getOrderedPropertySheets():
+                if psheet.hasProperty('fullname'):
+                    user_fn = psheet.getProperty('fullname')
+                    if user_fn:
+                        # do not search other sheets
+                        break
+                else:
+                    user_fn = user_id
+            entry = {
+                'id': user_id,
+                'fullname': user_fn,
+            }
+            ret.append(entry)
+        ret.sort(cmp=lambda x, y: \
+            x['fullname'].lower() > y['fullname'].lower() and 1 or -1)
+        return ret
+    
+    def _allocateFilter(self):
+        filter = self.widget.groupIdFilter
+        try:
+            filtertranslation = IGenericFilterTranslation(self.context)
+            filter = filtertranslation.translateToFilterDefinition(filter)
+        except ComponentLookupError:
+            pass
+        except TypeError, e:
+            if e[0] == 'Could not adapt':
+                pass
+            else:
+                raise        
+        if type(filter) in types.StringTypes:
+            filter = [filter,]
+        return filter
+
+    def _groupIdFilterMatch(self, gid, filter):
+        """Check if gid matches filter.
+        """
+        for fil in filter:
+            # wildcard match
+            if fil.find('*') != -1:
+                # all groups are affected
+                if fil == '*':
+                    return True
+                # wildcard matches like '*foo'
+                elif fil.startswith('*'):
+                    if gid.endswith(fil[1:]):
+                        return True
+                # wildcard matches like 'foo*'
+                elif fil.endswith('*'):
+                    if gid.startswith(fil[:-1]):
+                        return True
+                # wildacard matches like '*foo*'
+                else:
+                    if gid.find(fil[1:-1]) != -1:
+                        return True
+            # exact match
+            else:
+                if fil == gid:
+                    return True
+        return False
+    
+    def _reduceMembers(self, members, filter):
+        """Reduce members to match filter.
+        """
+        # TODO
+        return members
