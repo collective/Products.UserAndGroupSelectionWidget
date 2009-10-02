3440fc976879018fc127196b2e4ed20d2bf92c1c
jensens <jensens@12379f12-61e2-0310-a953-c5ab0a7db1c5>
2009-10-02 11:35:47 +0000

    ops, no idea why this didnt crashed earlier

--- src/Products/UserAndGroupSelectionWidget/__init__.py
+++ src/Products/UserAndGroupSelectionWidget/__init__.py
@@ -3,5 +3,4 @@ from interfaces import IGenericGroupTranslation
 from interfaces import IGenericFilterTranslation
 
 from Products.CMFCore.DirectoryView import registerDirectory
-from config import GLOBALS
-registerDirectory('skins', GLOBALS)
+registerDirectory('skins', globals())
