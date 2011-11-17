8f2b6426d25653996a796256e5ae8db2ccc3af4b
Rok Garbas <rok@garbas.si>
2011-11-17 06:17:06 +0100

    putting AT widget in at folder, some cleanup of code

--- setup.py
+++ setup.py
@@ -1,44 +1,57 @@
 # Copyright 2008-2009, BlueDynamics Alliance, Austria - http://bluedynamics.com
 # BSD derivative License 
 
-from setuptools import setup, find_packages
-import sys, os
+import sys
+from setuptools import setup
+from setuptools import find_packages
+
 
 version = '2.0.5dev'
-shortdesc = "Archetypes Widget for User and Group Selection, works with many users."
-longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
-license = open(os.path.join(os.path.dirname(__file__), 'LICENSE.txt')).read()
 
-setup(name='Products.UserAndGroupSelectionWidget',
-      version=version,
-      description=shortdesc,
-      long_description=longdesc + license,
-      classifiers=[
-      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-            'Development Status :: 5 - Production/Stable',
-            'Environment :: Web Environment',
-            'Framework :: Zope2',
-            'License :: OSI Approved :: BSD License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',            
-      ], 
-      keywords='zope zope2 plone archetypes widget user',
-      author='BlueDynamics Alliance',
-      author_email='dev@bluedynamics.com',
-      url='https://svn.plone.org/svn/archetypes/MoreFieldsAndWidgets/UserAndGroupSelectionWidget',
-      license='BSD',
-      packages=find_packages('src'),
-      package_dir = {'': 'src'},
-      namespace_packages=['Products', ],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          #'Products.Archetypes', # keep commented until we skip Plone 2.5 support
-          'bda.cache',
-          # Zope 2 dependencies are missing
-      ],
-      extras_require = {'test': ['plone.app.testing']},
-)
 
+if sys.version_info < (2 , 7):
+    extra_install_requires = ['ordereddict']
+else:
+    extra_install_requires = []
+
+
+setup(
+    name='Products.UserAndGroupSelectionWidget',
+    version=version,
+    description='Archetypes Widget for User and Group Selection, works '
+                'with many users.',
+    long_description=open('README.rst').read() + open('LICENSE.txt').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Zope2',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        ], 
+    keywords='zope zope2 plone archetypes z3cform widget user',
+    author='BlueDynamics Alliance',
+    author_email='dev@bluedynamics.com',
+    url='https://github.com/collective/Products.UserAndGroupSelectionWidget',
+    license='BSD',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages=['Products', ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.component',
+        'Zope2',
+        'AccessControl',
+        'Products.CMFCore',
+        'Products.CMFPlone',
+        'Products.PlonePAS',
+        'bda.cache',
+        ] + extra_install_requires,
+    extras_require = {
+        'test': ['unittest2', 'plone.app.testing'],
+        },
+    )
