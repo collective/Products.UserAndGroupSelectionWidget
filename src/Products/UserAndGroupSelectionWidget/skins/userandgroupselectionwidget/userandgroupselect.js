61a9f92497fe52eb71e0fc2d5d30c75acc91f7a1
JC Brand <jc@opkode.com>
2012-01-16 21:18:17 +0200

    Bugfix.
    When only a subset of options in the multi-selection box are chosen, then only
    they are saved. This is counter-intuitive, especially since there is a button
    to remove options. So the options that are there, should all be saved,
    regardless of whether they are highlighted or not.

--- src/Products/UserAndGroupSelectionWidget/skins/userandgroupselectionwidget/userandgroupselect.js
+++ src/Products/UserAndGroupSelectionWidget/skins/userandgroupselectionwidget/userandgroupselect.js
@@ -28,7 +28,7 @@ function userandgroupselect_openBrowser(portal_url,
 }
 
 function userandgroupselect_setEntry(id, value, fieldId, multi) {
-    if (multi == 0) {
+    if (multi === 0) {
         var field = document.getElementById(fieldId);
         var field_label = document.getElementById(fieldId + '_label');
         field.value = id;
@@ -50,7 +50,7 @@ function userandgroupselect_setEntry(id, value, fieldId, multi) {
 }
 
 function userandgroupselect_removeEntry(fieldId, multi) {
-    if (multi == 0) {
+    if (multi === 0) {
         var field = document.getElementById(fieldId);
         var field_label = document.getElementById(fieldId + '_label');
         field.value = '';
@@ -62,9 +62,20 @@ function userandgroupselect_removeEntry(fieldId, multi) {
                 list[x] = null;
             }
         }
-        // this seem not that senceful
-        // for (var x = 0; x < list.length; x++) {
-        //     list[x].selected = 'selected';
-        // }     
     }
 }
+
+jQuery(document).ready(function() {
+    // Make sure that all elements inside a multi-select widget is chosen.
+    // Otherwise, if only a subset is chosen, only those will be saved, which
+    // would confuse the hell out of users.
+    jQuery('select.usersandgroupsselect').each(function () {
+        var select = jQuery(this);
+        select.closest('form').submit(function (event) {
+            select.children('option').each(function () {
+                jQuery(this).attr('selected', 'selected');
+            });
+        });
+    });
+});
+
