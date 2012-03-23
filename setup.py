0b24ce6ae76939fdebeb25c7906f43f516ac2324
JC Brand <jc@opkode.com>
2012-03-23 15:57:42 +0200

    Preparing release 3.0beta1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-version = '3.0dev'
+version = '3.0beta1'
 
 if sys.version_info < (2, 7):
     extra_install_requires = ['ordereddict']
