9ab1cc58114b5f19c3aa602cd012e31323d2b961
Jens W. Klein <jens@bluedynamics.com>
2013-09-23 16:08:11 +0200

    prepare release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-version = '3.0b5dev'
+version = '3.0b5'
 
 if sys.version_info < (2, 7):
     extra_install_requires = ['ordereddict']
